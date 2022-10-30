eb8770eefd5fa99b28a687405387d320c4ca503c
lauvsong <hyehye9355@naver.com>
2022-10-30 20:51:08 +0900

    :sparkles: Add 15. 3Sum

---
+++ medium/15.py
@@ -0,0 +1,65 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        nums.sort()
+        bound = len(nums) - 1
+
+        for i, num in enumerate(nums):
+            if 0 < i and num == nums[i-1]:
+                continue
+
+            target = -num
+            left = i + 1
+            right = bound
+
+
+            while left < right:
+                res = nums[left] + nums[right]
+                if res < target:
+                    left += 1
+                elif res > target:
+                    right -= 1                
+                if res == target:
+                    ans.append((num, nums[left], nums[right]))
+                    left += 1
+                    
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+            
+        return ans
+        
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+"""
